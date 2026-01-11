090c26b9450a3d5a2716cc0ab99ebd2c5dec9917
pegab <p.gabriel@posteo.de>
2026-01-11 19:56:56 +0100

    changed var names

--- MultiTables/ContentView.swift
+++ MultiTables/ContentView.swift
@@ -30,85 +30,113 @@ struct ContentView: View {
   
     @State private var answerArr = [0,0,0]
     
+    @State private var rotation: Double = 0 //needed for spinning animation, start degree 0.0
+    
+
+    
 
     
     var body: some View {
         
-        Form {
-           
-            Text("\(randomInt) * \(userChoiseTable)").font(.title).frame(maxWidth: .infinity, alignment: .center)
-
-            VStack{
-                Text("Choose multiplication table")
-                                            .font(.headline)
-              
-                                        
-        Stepper("\(userChoiseTable) Table", value: $userChoiseTable, in: 2...12, step: 1)
-            }
+        NavigationStack{
+            
             
             VStack{
-                Text("How many Questions?").font(.headline)
                 
-                Picker("Flavor", selection: $numberOfQuestions) {
-                    ForEach(questionNumberArr, id: \.self) {
-                        Text("\($0)")
-                    }
-                }.pickerStyle(.segmented)
-                    .onChange(of: numberOfQuestions) {
-                        answerArr = buildButtonArray()
-                    }
-                    .onChange(of: userChoiseTable) {
-                        answerArr = buildButtonArray()
-                    }
+                
+                
+                Form {
                     
+                    
+                    VStack{
+                        
+                        Text("\(randomInt) * \(userChoiseTable)")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .rotationEffect(.degrees(rotation)) //needed for animation
+                            .animation(.smooth, value: rotation)// needed for animation
 
+                            
+                    }.listRowBackground(Color.orange.opacity(0.8)).padding()
+                        
+                    VStack(){
+                        Text("Choose multiplication table")
+                            .font(.headline)
+                        
+                        
+                        Stepper("\(userChoiseTable) Table", value: $userChoiseTable, in: 2...12, step: 1)
+                    }.listRowBackground(Color.green.opacity(0.4))
+                    
+                    VStack{
+                        Text("How many Questions?").font(.headline)
+                        
+                        Picker("Flavor", selection: $numberOfQuestions) {
+                            ForEach(questionNumberArr, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }.pickerStyle(.segmented)
+                            .onChange(of: numberOfQuestions) {
+                                answerArr = buildButtonArray()
+                            }
+                            .onChange(of: userChoiseTable) {
+                                answerArr = buildButtonArray()
+                            }
+                    }.listRowBackground(Color.blue.opacity(0.4))
+                }
+                Section{
+                    VStack{
+                        
+                        Text("Chose the correct answer")
+                            .font(.headline)
+                        
+                        
+                        HStack(spacing: 72) {
+                            ForEach(0..<3) {number in
+                                
+                                Button {
+                                    
+                                    buttonTapped(number)
+                                    
+                                }
+                                label: {
+                                    Text("\(answerArr[number])")
+                                }
+                                .frame(width: 60, height: 30)
+                                .foregroundStyle(.orange)
+                                .font(.title2)
+                                .background(Color.black)
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                                
+                            }
+                        }
+                    }.padding(20)
+                        .background(Color.red.gradient)
+                        .cornerRadius(16)
+                    
+                    Spacer()
+                }
+            }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: newQuestion)
+            } message: {
+                if userAnswer == randomInt * userChoiseTable {
+                    Text("\(alertTxt)")
+                } else {
+                    Text("\(alertTxt)")
+                }
             }
             
-       
-        }
-        
-        Text("Chose the correct answer")
-            .font(.headline)
-      
-        
-        HStack(spacing: 72) {
-            ForEach(0..<3) {number in
-                
-                Button {
-                    
-                    buttonTapped(number)
-                    
-                }
-                label: {
-                    Text("\(answerArr[number])")
-                }
-                .frame(width: 60, height: 30)
-                .foregroundStyle(.orange)
-                .font(.title2)
-                .background(Color.black)
-                .clipShape(.capsule)
-                .shadow(radius: 5)
-                
+            .alert(endTitle, isPresented: $showingEnd) {
+                Button("Continue", action: newGame)
+            } message: {
+                Text("You have finished")
             }
+            .scrollContentBackground(.hidden) // benötigt mann, damit der Hintergrund des Forms ausgeblendet wird.
+                            .background(.black.opacity(0.20))
+            .navigationTitle("MultiTables")
         }
-        
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: newQuestion)
-        } message: {
-            if userAnswer == randomInt * userChoiseTable {
-                Text("\(alertTxt)")
-            } else {
-                Text("\(alertTxt)")
-            }
-        }
-        
-        .alert(endTitle, isPresented: $showingEnd) {
-                    Button("Continue", action: newGame)
-                } message: {
-              Text("You have finished")
-        }
-        
-        
     }
     
     func buttonTapped(_ number: Int) {
@@ -133,8 +161,8 @@ struct ContentView: View {
         
         var sendArr = [Int]()
         sendArr.append(randomInt * userChoiseTable)
-        sendArr.append(randomInt + 5 * 2)
-        sendArr.append(userChoiseTable + 12)
+        sendArr.append(randomInt * 2 - 3)
+        sendArr.append(userChoiseTable * 4 + 1 )
         sendArr.shuffle()
         return sendArr
         
@@ -148,6 +176,7 @@ struct ContentView: View {
         randomInt = Int.random(in: 1...12)
         answerArr = buildButtonArray()
         answeredQuestions += 1
+        rotation += 360 //make it spin
         
         if numberOfQuestions == answeredQuestions {
             showingEnd = true
